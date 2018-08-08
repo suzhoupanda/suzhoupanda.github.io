f1cad2830bb0cd49501c2c7b613985007308cd7c
Alex Makedonski <alexmakedonski@Alexs-MacBook-Pro.local>
2018-08-08 16:00:20 +0800

    Added character images

--- js/asd_web.js
+++ js/asd_web.js
@@ -1,4 +1,9 @@
 
+//TODO: optimize by pre-loading images, define function on for the entire document scope
+
+//TODO: define class for character so that animations for the character can run and stopped more conveniently 
+
+
 $(document).ready(function(){
 
 	var targetArea = $("#target-area");
@@ -125,6 +130,22 @@ $(document).ready(function(){
 
 	}
 
+
+	var getCenterPositionInTargetArea = function(targetArea){
+
+		var offset = targetArea.offset();
+		var left = offset.left
+		var top = offset.top;
+		var width = targetArea.width()
+		var height = targetArea.height();
+
+		var randomLeft = Math.floor(width/2.0) + left;
+		var randomTop = Math.floor(height/2.0) + top;
+
+		return [randomLeft,randomTop];
+
+	}
+
 	var isOutsideTargetArea = function(targetArea, randomLeft,randomTop, randomHeight, randomWidth){
 
 
@@ -214,6 +235,287 @@ $(document).ready(function(){
 		return randomPositions;
 	}
 
+	var getRandomCrosshairImageName = function(){
+
+		var crosshairs = [
+			"crosshair_blue_large",
+			"crosshair_blue_small",
+			"crosshair_outline_large",
+			"crosshair_outline_small",
+			"crosshair_red_large",
+			"crosshair_red_small"
+		]
+
+		var randomIndex = Math.floor(Math.random()*crosshairs.length) 
+
+		return crosshairs[randomIndex];
+	}
+
+	var getCrossHairImagePath = function(){
+
+		 var basePath = "img/crosshair/";
+
+		 var imageName = getRandomCrosshairImageName();
+
+		 var imageExtension = ".png";
+
+		 return basePath + imageName + imageExtension;
+	}
+
+
+	var isToRight = function(object,clickX,clickY){
+
+		var offset = object.offset();
+		var left = offset.left;
+		var right = offset.left + object.width();
+
+		return clickX > right;
+
+	}
+
+	var isToLeft = function(object,clickX,clickY){
+
+		var offset = object.offset();
+		var left = offset.left;
+
+		return clickX < left;
+
+	}
+
+	var isAbove = function(object,clickX,clickY){
+
+		var offset = object.offset();
+		var top = offset.top;
+
+		return clickY < top;
+
+	}
+
+	var isBelow = function(object,clickX,clickY){
+
+		var offset = object.offset();
+		var top = offset.top;
+		var bottom = offset.top + object.height();
+
+		return clickY > bottom;
+
+	}
+
+	var moveCrossHair = function(crosshair,xPos,yPos){
+
+			if(isAbove(crosshair,xPos,yPos) && isToRight(crosshair,xPos,yPos)){
+				crosshair.css({
+					"left":"+=20px",
+					"top":"-=20px"
+				});
+			} else if(isAbove(crosshair,xPos,yPos) && isToLeft(crosshair,xPos,yPos)){
+				crosshair.css({
+					"left":"-=20px",
+					"top":"-=20px"
+				});
+			} else if(isBelow(crosshair,xPos,yPos) && isToRight(crosshair,xPos,yPos)){
+				crosshair.css({
+					"left":"+=20px",
+					"top":"+=20px"
+				});
+			} else if(isBelow(crosshair,xPos,yPos) && isToLeft(crosshair,xPos,yPos)){
+
+				crosshair.css({
+					"left":"-=20px",
+					"top":"+=20px"
+				});
+			}else if(isAbove(crosshair,xPos,yPos)){
+
+				crosshair.css({
+					"top":"-=20px"
+				});
+			} else if(isBelow(crosshair,xPos,yPos)){
+
+				crosshair.css({
+					"top":"+=20px"
+				});
+			} else if(isToRight(crosshair,xPos,yPos)){
+
+				crosshair.css({
+					"left":"+=20px"
+				});
+			} else if(isToLeft(crosshair,xPos,yPos)){
+
+				crosshair.css({
+					"left":"-=20px"
+				});
+			}
+	}
+
+	
+
+	var configureCrossHairForTargetAreaMovement = function(crosshair, targetArea){
+
+		targetArea.on("click",function(e){
+			var xPos = e.originalEvent.clientX;
+			var yPos = e.originalEvent.clientY;
+
+			console.log("X-Pos: " + xPos + ", Y-Pos: " + yPos);
+
+			moveCrossHair(crosshair,xPos,yPos);
+
+		
+		});
+	}
+
+	var appendCrosshair = function(targetArea){
+
+
+		var crossHairImagePath = getCrossHairImagePath();
+
+		var crosshair = $("<img>");
+
+		var centerPoint = getCenterPositionInTargetArea(targetArea);
+
+		var styles = {
+			"position":"absolute",
+			"left": centerPoint[0] + "px",
+			"top": centerPoint[1] + "px"
+		}
+
+		crosshair.attr("src",crossHairImagePath).css(styles).on("click",function(){
+			console.log("Crosshair has been clicked");
+
+			
+			
+
+		});
+
+		configureCrossHairForTargetAreaMovement(crosshair,targetArea);
+
+
+		targetArea.append(crosshair);
+
+	}
+
+
+	var configureAnimation = function(character,imageBasePath,imageExtension,textureNames,animationInterval){
+
+		if(!character.is("img")){
+			console.log("Error: could not configure animation for this character because it is not an img tag");
+			return;
+		}
+
+		var i = 0;
+
+		var animationID = setInterval(function(){
+
+			if(i < textureNames.length){
+				character.attr("src",imageBasePath+textureNames[i]+imageExtension);
+				i++;
+			} else {
+				i = 0;
+			}
+
+
+		},animationInterval);
+
+		return animationID;
+	}
+
+	var configureDefaultTexture = function(character,imageBasePath,textureName,imageExtension){
+
+		if(!character.is("img")){
+			console.log("Error: could not configure animation for this character because it is not an img tag");
+			return;
+		}
+		
+			
+		character.attr("src",imageBasePath+textureName+imageExtension);
+		
+	}
+
+	var configureCharacterBaseStyles = function(character){
+		
+		var baseStyles = {
+			"position":"absolute"
+		}
+
+		character.css(baseStyles);
+	}
+
+
+	var createSpikeball = function(){
+		var spikeball = $("<img>");
+
+		configureCharacterBaseStyles(spikeball);
+
+		var basePath = "./img/characters/";
+
+		var textures = [
+			"spikeBall2",
+			"spikeBall1"
+		]
+
+		var imageExtension = ".png";
+
+		configureDefaultTexture(spikeball,basePath,textures[0],imageExtension);
+
+		var spikeBallAnimationID = configureAnimation(spikeball,"/img/characters/",".png",textures,3000);
+
+		return spikeball;
+	}
+
+	var createFlyman = function(){
+		var flyMan = $("<img>");
+
+		configureCharacterBaseStyles(flyMan);
+
+		var basePath = "./img/characters/";
+
+		var textures = [
+			"flyMan1",
+			"flyMan2",
+			"flyMan3",
+			"flyMan2",
+			"flyMan1"
+		]
+
+		var imageExtension = ".png";
+
+		configureDefaultTexture(flyMan,basePath,textures[0],imageExtension);
+
+
+		var flymanAnimation2D = configureAnimation(flyMan,"/img/characters/",".png",textures,3000);
+
+		return flyMan;
+
+	}
+
+	var appendCharacterAtRandomPosition = function(targetArea,characterName){
+
+		var character;
+
+		switch(characterName){
+			case "flyman":
+				character = createFlyman();
+				break;
+			case "spikeball":
+				character = createSpikeball();
+				break;
+			default:
+				character = createSpikeball();
+				break;
+		}
+
+		var randomPos = getRandomPositionInTargetArea(targetArea);
+		var randomLeft = randomPos[0];
+		var randomTop = randomPos[1];
+
+		var positionInfo = {
+			"left": randomLeft + "px",
+			"top" : randomTop + "px"
+		}
+
+		character.css(positionInfo);
+
+		targetArea.append(character);
+	}
 
 
 	clearPositionDataFromLocalStorage();
@@ -227,6 +529,9 @@ $(document).ready(function(){
 			console.log(randomPositions[index]);
 		}
 
+		appendCrosshair(targetArea);
+
+		appendCharacterAtRandomPosition(targetArea,"flyman");
 
 
 	});
